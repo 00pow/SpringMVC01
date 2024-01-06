db4d24ec15c853330a8f90945f04c09640f37121
00pow <kimseo131612@gmail.com>
2024-01-07 00:51:42 +0900

    lombok!!

--- SpringMVC01/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/board/controller/BoardController.java
@@ -3,32 +3,25 @@ package kr.board.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 
 	// /boardList.do
+	@Autowired
+	private BoardMapper mapper;
+	
 	// HandlerMapping
 	@RequestMapping("/boardList.do")
 	public String boardList(Model model) {
-		
-		Board vo=new Board();
-		vo.setIdx(1);
-		vo.setTitle("게시판실습");
-		vo.setContent("게시판실습");
-		vo.setWriter("김서영");
-		vo.setIndate("2024-01-04");
-		vo.setCount(0);
-		
-		List<Board> list = new ArrayList<Board>();
-		list.add(vo);
-		list.add(vo);
-		list.add(vo);
+		List<Board> list=mapper.getLists();
 		model.addAttribute("list", list);
 		return "boardList"; // /WEB-INF/views/boardList.jsp -> forward
 	}
--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,9 +2,17 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 
-// @Mapper - Mybatis API
+@Mapper//- Mybatis API
 public interface BoardMapper {
 	public List<Board> getLists(); // 전체리스트 가져오는 메소드
+	public void boardInsert(Board vo); 
+    public Board boardContent(int idx);
+    
+    public void boardDelete(int idx);
+    
+    public void boardUpdate(Board vo);
 }
